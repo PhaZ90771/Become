1fe8c8f971effa5970ac9661e9c8cbab0bc03054
Benjamin Shutt <shuttben@gmail.com>
2020-05-20 19:23:51 -0700

    Changed around how look at works to solve some targeting issues.

---
+++ Assets/Scripts/CreatureController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using UnityEngine;
+
+public class CreatureController : MonoBehaviour
+{
+    private bool isGrounded = true;
+
+    private Vector3 relativeBottom;
+    private Vector3 bottom;
+    private Vector3 groundCheckMargin = new Vector3(0f, 0.01f, 0f);
+    private Vector3 direction;
+    private Vector3 velocity;
+
+    private Rigidbody creatureRigidBody;
+
+    [SerializeField]
+    private CharacterControlConstants characterConstants = new CharacterControlConstants
+    {
+        GroundAccelerationTime = 3f,
+        GroundDecelerationTime = 0.25f,
+        GroundCheckDistance = 0.1f,
+        GroundSpeedMax = 5f,
+    };
+
+    private void Awake()
+    {
+        creatureRigidBody = GetComponent<Rigidbody>();
+        relativeBottom = GetRelativeBottomPoint();
+    }
+
+    private void Update()
+    {
+        GroundCheck();
+
+        if (isGrounded)
+        {
+            var targetVelocity = direction * characterConstants.GroundSpeedMax;
+            var t = Time.deltaTime;
+            t /= direction.magnitude > 0 ? characterConstants.GroundAccelerationTime : characterConstants.GroundDecelerationTime;
+            velocity = Vector3.Lerp(velocity, targetVelocity, t);
+        }
+
+        creatureRigidBody.velocity = velocity;
+    }
+
+    private Vector3 GetRelativeBottomPoint()
+    {
+        var filter = GetComponent<MeshFilter>();
+
+        if (filter == null || filter.mesh == null)
+            return Vector3.zero;
+
+        var centerY = filter.mesh.bounds.center.y;
+        var relativeY = centerY - filter.mesh.bounds.extents.y;
+
+        return new Vector3(0, relativeY, 0);
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = false;
+        bottom = transform.position + relativeBottom;
+
+        Ray ray = new Ray(bottom + groundCheckMargin, Vector3.down);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            isGrounded = hit.distance < characterConstants.GroundCheckDistance;
+            Debug.DrawRay(bottom, Vector3.down, Color.green);
+            return;
+        }
+
+        Debug.DrawRay(bottom, Vector3.down, Color.red);
+    }
+
+    public void SetRotation(Quaternion newRotation)
+    {
+        transform.rotation = newRotation;
+    }
+
+    public Quaternion GetRotation()
+    {
+        return transform.rotation;
+    }
+
+    public void SetInputDirection(Vector3 inputDirection)
+    {
+        direction = transform.TransformDirection(inputDirection);
+    }
+
+    public Vector3 GetVelocity()
+    {
+        return creatureRigidBody.velocity;
+    }
+
+    public LookAt HostTakeOver()
+    {
+        tag = "Player";
+        gameObject.layer = 8;
+        return GetComponentInChildren<LookAt>();
+    }
+
+    public void HostRelease()
+    {
+        tag = "Creature";
+        gameObject.layer = 0;
+    }
+
+    [Serializable]
+    struct CharacterControlConstants
+    {
+        public float GroundAccelerationTime;
+        public float GroundDecelerationTime;
+        public float GroundCheckDistance;
+        public float GroundSpeedMax;
+    }
+}
--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -1,34 +1,15 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(PlayerInputManager))]
 public class PlayerCharacterController : MonoBehaviour
 {
-    static int layerMask = ~(1 << 9);
+    static readonly int layerMask = ~(1 << 9);
 
     private PlayerInputManager playerInputManager;
 
-    private Transform host;
-    private Rigidbody hostRigidbody;
+    private CreatureController host;
 
-    private bool isGrounded = true;
     private Transform target;
-    private Vector3 relativeBottom;
-    private Vector3 bottom;
-
-    private Vector3 groundCheckMargin = new Vector3(0f, 0.01f, 0f);
-
-    [SerializeField]
-    private Vector3 Velocity;
-
-    [SerializeField]
-    private CharacterControlConstants characterConstants = new CharacterControlConstants
-    {
-        GroundAccelerationTime = 3f,
-        GroundDecelerationTime = 0.25f,
-        GroundCheckDistance = 0.1f,
-        GroundSpeedMax = 5f,
-    };
 
     private void Awake()
     {
@@ -38,41 +19,13 @@ public class PlayerCharacterController : MonoBehaviour
 
     private void Update()
     {
-        GroundCheck();
         TargetCheck();
 
         var rotation = Vector3.zero;
         rotation.y = playerInputManager.GetCameraRotation();
-        host.rotation = Quaternion.Euler(rotation);
+        host.SetRotation(Quaternion.Euler(rotation));
 
-        var direction = host.TransformDirection(playerInputManager.GetMovementInput());
-
-        if (isGrounded)
-        {
-            var targetVelocity = direction * characterConstants.GroundSpeedMax;
-            var t = Time.deltaTime;
-            t /= direction.magnitude > 0 ? characterConstants.GroundAccelerationTime : characterConstants.GroundDecelerationTime;
-            Velocity = Vector3.Lerp(Velocity, targetVelocity, t);
-        }
-
-        hostRigidbody.velocity = Velocity;
-    }
-
-    private void GroundCheck()
-    {
-        isGrounded = false;
-        bottom = host.position + relativeBottom;
-
-        Ray ray = new Ray(bottom + groundCheckMargin, Vector3.down);
-
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            isGrounded = hit.distance < characterConstants.GroundCheckDistance;
-            Debug.DrawRay(bottom, Vector3.down, Color.green);
-            return;
-        }
-
-        Debug.DrawRay(bottom, Vector3.down, Color.red);
+        host.SetInputDirection(playerInputManager.GetMovementInput());
     }
 
     private void TargetCheck()
@@ -90,42 +43,17 @@ public class PlayerCharacterController : MonoBehaviour
         }
     }
 
-    private Vector3 GetRelativeBottomPoint()
-    {
-        var filter = host.GetComponent<MeshFilter>();
-
-        if (filter == null || filter.mesh == null)
-            return Vector3.zero;
-
-        var centerY = filter.mesh.bounds.center.y;
-        var relativeY = centerY - filter.mesh.bounds.extents.y;
-
-        return new Vector3(0, relativeY, 0);
-    }
-
     private bool SetHost(Transform newHost)
     {
         if (newHost.CompareTag("Creature"))
         {
-            var oldHost = host;
-            host = newHost;
+            if (host != null) host.HostRelease();
+            host = newHost.GetComponent<CreatureController>();
+            playerInputManager.playerCamera.LookAt = host.HostTakeOver().transform;
 
-            if (oldHost != null)
-            {
-                oldHost.tag = "Creature";
-                oldHost.gameObject.layer = 0;
-            }
+            transform.SetParent(host.transform, worldPositionStays: false);
 
-            host.tag = "Player";
-            host.gameObject.layer = 8;
-
-            hostRigidbody = host.GetComponent<Rigidbody>();
-            relativeBottom = GetRelativeBottomPoint();
-
-            transform.SetParent(host, worldPositionStays: false);
-
-            playerInputManager.playerCamera.m_XAxis.Value = host.rotation.eulerAngles.y;
-            Velocity = hostRigidbody.velocity;
+            playerInputManager.playerCamera.m_XAxis.Value = host.GetRotation().eulerAngles.y;
 
             return true;
         }
@@ -150,14 +78,5 @@ public class PlayerCharacterController : MonoBehaviour
         {
             Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward);
         }
-    }
-
-    [Serializable]
-    struct CharacterControlConstants
-    {
-        public float GroundAccelerationTime;
-        public float GroundDecelerationTime;
-        public float GroundCheckDistance;
-        public float GroundSpeedMax;
     }
 }
